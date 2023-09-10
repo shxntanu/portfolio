4d5721074173786e02bfee55045b767640be30e5
Shantanu <shantanuwable2003@gmail.com>
2023-09-10 21:46:00 +0530

    Edited SEO info

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -8,8 +8,8 @@ export function useSeoProps(
 ): Partial<ComponentProps<typeof NextSeo>> {
 	const router = useRouter();
 
-	const title = 'nuro ─ developer';
-	const description = "Hey 👋 I'm Ben, a developer";
+	const title = 'Shantanu ─ developer';
+	const description = "Hey 👋 I'm Shantanu, a developer";
 
 	return {
 		title,
